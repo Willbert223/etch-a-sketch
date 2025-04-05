d7aa9195dc9d0c58e97c13b91331bef701868e5b
willbert978 <wcameron2003@gmail.com>
2025-04-05 02:03:16 -0500

    Got the hover effect working in javascript no css requried in file at least

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const container = document.getElementById("container");
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap'
-container.style.boxSizing = 'border-box';
 
-const hover = document.querySelector('.hover');
-
-hover.addEventListener('hover', (color) => {
-    onmouseover = fnPickColor(newDiv);
-})
 
 for(let i = 0; i < 256; i++){
     const newDiv = document.createElement('div');
@@ -16,8 +10,30 @@ for(let i = 0; i < 256; i++){
     newDiv.style.height = '100px';
     newDiv.style.border = '1px solid black';
     newDiv.style.aspectRatio = '1 / 1';
-    newDiv.style.background = <COLOR RANDOM GENERATED COLOR>
+    changeBackgroundColorOnHover(newDiv);
     container.appendChild(newDiv);
-
+    
+    
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+    
+      function changeBackgroundColorOnHover(newDiv) {
+        const originalBackgroundColor = newDiv.style.backgroundColor;
+      
+        newDiv.addEventListener('mouseover', function() {
+          newDiv.style.backgroundColor = getRandomColor();
+        });
+      
+        newDiv.addEventListener('mouseout', function() {
+          newDiv.style.backgroundColor = originalBackgroundColor || 'hsl(0, 0%, 70%)';
+        });
+      }
+      
 
 }
