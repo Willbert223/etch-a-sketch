5a955ea48842a975ad9274e6cfd76322fe5249da
willbert978 <wcameron2003@gmail.com>
2025-04-27 02:25:22 -0500

    Made the divs center in the screen and they change color when you mouseover and out sends them back to original

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 // create 16 div elements located inside a container
-const container = document.getElementById('container');
+const container = document.getElementById('divContainer');
 
 for (let i = 0; i < 16; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
 }
 
-// make the divs appear using flexbox aligned in grid format
+
+
 // box the divs with a border in the center of the screen
 // make the divs change color when the mouse goes over them and back when out
+const divs = document.querySelectorAll('#divContainer div');
+
+divs.forEach(div => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'blue';
+    });
+    div.addEventListener('mouseout', () => {
+        div.style.backgroundColor = 'lightgray';
+    });
+});
+
 
 // ask user for number of divs per side
 // once input is recived remove current grid and generate new grid
